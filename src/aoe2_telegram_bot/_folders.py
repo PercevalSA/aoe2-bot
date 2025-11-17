65189d505d91ae089708369f4da3f9766d1f0178
PercevalSA <perceval.arenou@gmail.com>
2025-11-17 08:51:17 +0000

    fix audio download url

--- src/aoe2_telegram_bot/_folders.py
+++ src/aoe2_telegram_bot/_folders.py
@@ -6,7 +6,9 @@ audio_caption = images_folder / "aok-ico.png"
 audio_folder = current_module_folder / "audio"
 bootstrap_file = audio_folder / "installation_complete"
 
-audio_url = "https://media.githubusercontent.com/media/PercevalSA/aoe2-bot/main/audio/"
+audio_url = (
+    "https://media.githubusercontent.com/media/PercevalSA/aoe2-telegram-bot/main/audio/"
+)
 audio_archives = [
     "civilization.zip",
     "sound.zip",
